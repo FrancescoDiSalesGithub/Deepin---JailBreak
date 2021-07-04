84fc0b781ad1b2858dc47e0104bc00ded6469de6
Francesco Di Sales <f.disales@hotmail.com>
2021-07-04 17:39:07 +0200

    Update removeService.sh

--- script/removeService.sh
+++ script/removeService.sh
@@ -1,2 +1,11 @@
-sudo service ssh stop
-sudo service smbd stop
+isRoot='id -u'
+
+if [ isRoot !=0 ]
+then
+  echo "run this script as root"
+else
+#remove ssh service from first installation
+sudo systemctl disable ssh.service
+sudo systemctl disable smbd.service
+
+fi
